3d12cd7e50d4e741621628a2dfda7a6632426815
Nhuttruong61 <Nhuttruong2261@gmail.com>
2024-04-02 22:13:39 +0700

    build CRUD doctor

--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,13 @@
 const auth = require("./auth");
 const user = require("./user");
 const department = require("./department");
+const doctor = require("./doctor");
 
 const initialRouter = (app) => {
   app.use("/api/v1/auth", auth);
   app.use("/api/v1/user", user);
   app.use("/api/v1/department", department);
-
+  app.use("/api/v1/doctor", doctor);
   app.use("/", (req, res) => {
     return res.send("Server on");
   });
